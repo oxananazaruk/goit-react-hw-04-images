26fbf3445692470cfa6d13826c49984ace0e7356
oxananazaruk <132213141+oxananazaruk@users.noreply.github.com>
2023-11-19 20:19:59 +0200

    updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { GlobalStyle } from '../GlobalStyle';
 import toast, { Toaster } from 'react-hot-toast';
 import { SearchBar } from './Searchbar/Searchbar';
@@ -8,80 +8,67 @@ import { Button } from './Button/Button';
 import { Loader } from './Loader/Loader'
 import {Error} from './Error/Error'
 
-export class App extends Component {
-  state = {
-    images: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    error: false,
-    totalPages: 0,
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [totalPages, setTotalPages] = useState(0);
+ 
+ useEffect(() => {
+   const userQuery = query.slice((query.indexOf("/") + 1), query.length);
+   if ( userQuery.trim() === '') {
+         return;
+     };
+   const onFetchPhotos = async () => { 
+     setIsLoading(true);
+     setError(false);
+     const responce = await fetchPhotos(userQuery, page);
+     const { hits, totalHits } = responce;
+     if (hits.length === 0) {
+         return toast.error("Sorry, there are no images matching your search query. Please try again.");
+     };
+     setImages(prevImages => ([...prevImages, ...hits]));
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    const userQuery = query.slice((query.indexOf("/") + 1), query.length);
-     if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-     ) { 
-      try {
-      this.setState({ isLoading: true, error: false });
-        const responce = await fetchPhotos(userQuery, page);
-        const { hits, totalHits } = responce;
-        if (hits.length === 0 || userQuery.trim() === '') {
-        return  toast.error("Sorry, there are no images matching your search query. Please try again.");
-    };
+     if (page === 1) {
+         toast(`Hooray! We found ${totalHits} images.`);
+       }
         
-        this.setState(prevState => {
-          return {
-            images: [...prevState.images, ...hits],
-            isLoading: false,
-            totalPages: Math.ceil(totalHits / 12),
-          }
-        });
-        if (page === 1) {
-          toast(`Hooray! We found ${totalHits} images.`);
-        }
-        
-    } catch (error) {
-      this.setState({ error: true });
-    } finally {
-      this.setState({ isLoading: false });
-    }
-    }
-  }
+     setIsLoading(false);
+     setTotalPages(Math.ceil(totalHits / 12));
+   }
 
-  handleSubmit = newQuery => {
+     try {
+      onFetchPhotos();
+     } catch (error) {
+       setError(true);
+     } finally {
+       setIsLoading(false);
+     }
+  }, [query, page, error ]);
+
+  const handleSubmit = newQuery => {
     const userRequest = newQuery.query;
-    this.setState({
-      query: `${Date.now()}/${userRequest}`,
-      page: 1,
-      images: [],
-      totalPages: 0,
-    });
+    setQuery(`${Date.now()}/${userRequest}`);
+    setPage(1);
+    setImages([]);
+    setTotalPages(1);
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => {
-      return {
-        page: prevState.page + 1,
-      };
-    });
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { images, isLoading, error, page, totalPages } = this.state;
     return (
       <div>
-        <SearchBar onSubmit={this.handleSubmit} />
+        <SearchBar onSubmit={handleSubmit} />
         {error && <Error/>}
         <ImageGallery items={images} />
-        {images.length > 0 && (page === totalPages ?  <p>We're sorry, but you've reached the end of search results.</p> : <Button onLoad={this.handleLoadMore} />)}
+        {images.length > 0 && (page === totalPages ?  <p>We're sorry, but you've reached the end of search results.</p> : <Button onLoad={handleLoadMore} />)}
         {isLoading && <Loader />}
         <GlobalStyle />
         <Toaster/>
       </div>
     );
-  }
 };
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,38 +1,28 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { ImageModal } from '../Modal/Modal';
 import { GalleryItem, GalleryItemImage } from './ImageGalleryItem.styled';
 
-export class ImageGalleryItem extends Component {
-  state = {
-    isModalOpen: false,
+export const ImageGalleryItem = ({ image, name, largeImg }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
   };
 
-  openModal = () => {
-    this.setState({
-      isModalOpen: true,
-    });
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
-  closeModal = () => {
-    this.setState({
-      isModalOpen: false,
-    });
-  };
-
-  render() {
-    const { isModalOpen } = this.state;
-    const { image, name, largeImg } = this.props;
-    return (
-      <>
-        <GalleryItem onClick={this.openModal}>
-          <GalleryItemImage src={image} alt={name} />
-        </GalleryItem>
-        <ImageModal
-          isOpen={isModalOpen}
-          onClose={this.closeModal}
-          modalImg={largeImg}
-        />
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <GalleryItem onClick={openModal}>
+        <GalleryItemImage src={image} alt={name} />
+      </GalleryItem>
+      <ImageModal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        modalImg={largeImg}
+      />
+    </>
+  );
+};
